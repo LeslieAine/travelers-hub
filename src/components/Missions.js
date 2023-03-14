251116b0c6076de38612596e2c24880c2743a089
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-03-14 13:49:11 +0200

    fetch missions and storing them

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,16 @@
-const Missions = () => (
-  <div>My Profile</div>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions, missionsState } from '../redux/missions/missionsSlice';
+
+const Missions = () => {
+  const dispatch = useDispatch();
+  const { status } = useSelector(missionsState);
+
+  if (status === 'idle') {
+    dispatch(fetchMissions());
+  }
+  return (
+    <div>To be filled with missions</div>
+  );
+};
 
 export default Missions;
