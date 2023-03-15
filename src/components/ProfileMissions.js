a7a7a2021c3c3ee261409b62b5691a8788bca773
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-03-15 15:40:33 +0200

    change no missions case

--- src/components/ProfileMissions.js
+++ src/components/ProfileMissions.js
@@ -4,12 +4,15 @@ import styles from '../styles/ProfileMissions.module.css';
 
 const ProfileMissions = () => {
   const { missions } = useSelector(missionsState);
-  if (missions.length === 0) { return (<p>No joined missions yet</p>); }
+  const joinedMissions = missions
+    .filter((mission) => mission.reserved);
+
+  if (joinedMissions.length === 0) { return (<p>No joined missions yet</p>); }
+
   return (
     <ul className={styles['missions-list']}>
       <p className={styles.title}>My Missions</p>
-      {missions
-        .filter((mission) => mission.reserved)
+      {joinedMissions
         .map((mission) => (
           <li className={styles.mission} key={mission.mission_id}>
             {mission.mission_name}
